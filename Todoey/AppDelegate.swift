59ce01c6704a17a8eb363794bbfedf638d78ed4b
MikitaYurkouski <Jalapa10@yandex.ru>
2023-03-30 23:32:42 +0300

    Add Realm library

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import CoreData
+import RealmSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        print(Realm.Configuration.defaultConfiguration.fileURL)
+        
+        let data = Data()
+        data.name = "Mikita"
+        data.age = 22
+        
+        do {
+            let realm = try Realm()
+            try realm.write {
+                realm.add(data)
+            }
+        } catch {
+            print("Error initialising new realm, \(error)")
+        }
         
         
         return true
